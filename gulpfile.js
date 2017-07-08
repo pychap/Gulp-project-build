d6cea2efe29c23812e3d6816c5e180452713629e
Peter Chapman <pychapster@gmail.com>
2017-07-08 10:08:16 -0700

    Adding concatenated javascript files

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,16 @@
 var gulp = require('gulp'),
     gutil = require('gulp-util'),
-    coffee = require('gulp-coffee');
+    coffee = require('gulp-coffee'),
+    concat = require('gulp-concat');
 
-var coffeeSources = ['components/coffee/tagline.coffee']
+var coffeeSources = ['components/coffee/tagline.coffee'];
+// crreate array for this var
+var jsSources = [
+  'components/scripts/rclick.js',
+  'components/scripts/pixgrid.js',
+  'components/scripts/tagline.js',
+  'components/scripts/template.js'
+]
 
 gulp.task('coffee', function(){
   gulp.src(coffeeSources)
@@ -13,3 +21,9 @@ gulp.task('coffee', function(){
       // will move new file to scripts folder
         .pipe(gulp.dest('components/scripts'))
 });
+
+gulp.task('js', function(){
+  gulp.src(jsSources)
+    .pipe(concat('script.js'))
+    .pipe(gulp.dest('builds/development/js'))
+})

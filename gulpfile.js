dfd0a2a28767679ec6c7367aa33601b5875217bb
Peter Chapman <pychapster@gmail.com>
2017-07-09 16:29:01 -0700

    Created default task in gulpfile.js line 42

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,42 @@
 var gulp = require('gulp'),
     gutil = require('gulp-util'),
-    browserify = require('gulp-browserify'),
     coffee = require('gulp-coffee'),
+    browserify = require('gulp-browserify'),
+    compass = require('gulp-compass'),
     concat = require('gulp-concat');
 
 var coffeeSources = ['components/coffee/tagline.coffee'];
-// crreate array for this var
 var jsSources = [
   'components/scripts/rclick.js',
   'components/scripts/pixgrid.js',
   'components/scripts/tagline.js',
   'components/scripts/template.js'
-]
+];
+var sassSources = ['components/sass/style.scss'];
 
-gulp.task('coffee', function(){
+gulp.task('coffee', function() {
   gulp.src(coffeeSources)
-  // pipe file to coffe var...
-    .pipe(coffee({bare: true})
-      // in case of error
-        .on('error', gutil.log))
-      // will move new file to scripts folder
-        .pipe(gulp.dest('components/scripts'))
+    .pipe(coffee({ bare: true })
+      .on('error', gutil.log))
+    .pipe(gulp.dest('components/scripts'))
 });
 
-gulp.task('js', function(){
+gulp.task('js', function() {
   gulp.src(jsSources)
     .pipe(concat('script.js'))
     .pipe(browserify())
     .pipe(gulp.dest('builds/development/js'))
-})
+});
+
+gulp.task('compass', function() {
+  gulp.src(sassSources)
+    .pipe(compass({
+      sass: 'components/sass',
+      image: 'builds/development/images',
+      style: 'expanded'
+    })
+    .on('error', gutil.log))
+    .pipe(gulp.dest('builds/development/css'))
+});
+
+gulp.task('default', ['coffee', 'js', 'compass']);
